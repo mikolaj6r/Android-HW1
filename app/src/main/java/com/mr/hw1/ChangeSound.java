bd0a23684253e18735b56244812c27b6020be969
Mikołaj <mikolaj6r@gmail.com>
2019-04-01 21:15:10 +0200

    Add changeContact and changeSound

---
+++ app/src/main/java/com/mr/hw1/ChangeSound.java
@@ -0,0 +1,17 @@
+package com.mr.hw1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ChangeSound extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_sound);
+    }
+
+    public void onRadioButtonClicked(View view) {
+    }
+}
